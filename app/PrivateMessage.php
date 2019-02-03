8dc9d4aa96766f88599b30ab724c4e364bd73101
hassanhitleap1 <hassanhitleap1@gmail.com>
2019-02-03 13:23:47 -0800

    damy

--- app/PrivateMessage.php
+++ app/PrivateMessage.php
@@ -3,8 +3,32 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class PrivateMessage extends Model
 {
-    //
+    protected $fillable=[
+        'sender_id', 'received_id', 'subject', 'message', 'read'
+    ];
+
+
+    protected $appends = ['sender_id', 'received_id'];
+
+
+
+    public function getSenderAttribute()
+    {
+        return User::where('id', $this->sender_id)->first();
+    }
+
+
+    public function getReceivedAttribute()
+    {
+        return User
+        ::where('id', $this->sender_id)->first();
+    }
+
+    public function getCreatedAtAttribute($value){
+        return Carbon::parse($value)->diffForHumans();
+    }
 }
